3195f5dceab8208e8cecc895d70039c16bb7fee0
mlussati <mlussati@hotmail.com>
2022-12-04 19:28:37 -0300

    List Unpacking

---
+++ tips/list_unpack.py
@@ -0,0 +1,13 @@
+minha_list = [10, 20, 30, 40]
+a, *b = minha_list
+
+print(f"Variavel A = {a}")
+print(f"Variavel B = {b}")
+
+
+minha_list2 = ["yellow", "gray", "blue", "pink", "brown"]
+a, *b, c = minha_list2
+
+print(f"Variavel A = {a}")
+print(f"Variavel B = {b}")
+print(f"Variavel C = {c}")
